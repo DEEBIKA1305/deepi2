e6819757ee0e9b72f9f02d89ed26a7375e3c61f8
DEEBIKA1305 <tdeepika4eee@gmail.com>
2017-06-08 15:50:36 +0530

    Add files via upload

---
+++ primenum.java
@@ -0,0 +1,22 @@
+package dp;
+import java.util.*;
+public class primenum {
+public static void main(String args[]){
+	Scanner s=new Scanner(System.in);
+	int num,count=0;
+	System.out.println("Enter the number:");
+	num=s.nextInt();
+	for(int i=1;i<=num;i++){
+		if(num%i==0){
+			count++;
+		}
+		}
+	if(count==2){
+		System.out.println(num+"is a prime number");
+	}
+	else
+	{
+		System.out.println(num+"is not a prime number");
+	}
+}
+}
