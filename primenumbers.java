974213e4b465995acdff3e2eac8c9db7c960035c
DEEBIKA1305 <tdeepika4eee@gmail.com>
2017-06-09 14:15:44 +0530

    Add files via upload

---
+++ primenumbers.java
@@ -0,0 +1,21 @@
+package deepika;
+import java.util.*;
+public class primenumbers {
+
+	public static void main(String []args){
+	    int k=0;
+	    System.out.println("Prime numbers:");
+		for(int i=10;i<=99;i++){
+	    int a=i/2;
+	    for(int j=2;j<=a;j++){
+	    if(i%j==0){
+	           k=1;
+	           break;
+	    		}}
+	   	if(k==0){
+	   		System.out.print(i+" ");
+	   	}
+	   	k=0;
+	   	}
+		}
+}
