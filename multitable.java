0d2b11a01381972bd2df5f832b8cc2889b7d66f1
DEEBIKA1305 <tdeepika4eee@gmail.com>
2017-06-12 14:47:35 +0530

    Add files via upload

---
+++ multitable.java
@@ -0,0 +1,15 @@
+package dp;
+import java.util.*;
+public class multitable {
+	public static void main(String args[]){
+		Scanner s=new Scanner(System.in);
+		System.out.println("Enter a multiplier");
+		int num=s.nextInt();
+		for(int i=1;i<=15;i++){
+			int n=num*i;
+			System.out.println(n);
+		
+		}
+	}
+
+}
